a5ed9d4e536454d23cca8d8e185a6a3692a433c6
pawaniitd <pawan.samdani@gmail.com>
2013-10-17 23:26:29 +0530

    Basic HTML

--- index.php
+++ index.php
@@ -12,16 +12,35 @@
             	<h1>Welcome to the Beta-Gal App</h1>
                 <h3>Store and view your beta gal results online.</h3>
             </hgroup>
+			
+			<nav>
+				<a href="index.php">Home</a>
+			</nav>
         </header>
         
         <nav>
         	<h2>Add</h2>
             <ul>
-            	<li><a href="plasmid.php">Plasmid</a></li>
-                <li><a href="strain.php">Strain</a></li>
-                <li><a href="colony.php">Colony</a></li>
-                <li><a href="experiment.php">Experiment</a></li>
+            	<li><a href="index.php?q=plasmid">Plasmid</a></li>
+                <li><a href="index.php?q=strain">Strain</a></li>
+                <li><a href="index.php?q=experiment">Experiment</a></li>
             </ul>
         </nav>
+		
+		<?php
+			if (isset($_GET['q'])) {
+				switch ($_GET['q']) {
+					case "plasmid":
+						include 'includes/plasmid_form.html';
+						break;
+					
+					case "strain":
+						break;
+						
+					case "experiment":
+						break;
+				}
+			}
+		?>
     </body>
 </html>
